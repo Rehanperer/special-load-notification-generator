a314b3fbb6c9f80378ed2bebddf1a26f062bd331
Rehan Perera <pererarehan2007@gmail.com>
2026-01-28 21:04:58 +0300

    Updates

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/special-load-notification-generator/sw.js', { scope: '/special-load-notification-generator/' })})}
